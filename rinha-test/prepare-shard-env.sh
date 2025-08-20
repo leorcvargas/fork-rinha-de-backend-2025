d310928961942ef4956028bd90caaed7f8947ead
Leonardo Vargas <leocarvalho.vargas@gmail.com>
2025-08-20 18:16:32 -0300

    fix k6 install

--- rinha-test/prepare-shard-env.sh
+++ rinha-test/prepare-shard-env.sh
@@ -41,15 +41,10 @@ echo "✅ Docker installed successfully."
 echo ""
 
 # --- 3. Install k6 ---
-echo ">>> Step 3: Setting up k6 repository and installing k6..."
-# Add the k6 GPG key
-curl -fsSL https://k6.io/repo/gpg.key | sudo gpg --dearmor -o /usr/share/keyrings/k6-archive-keyring.gpg
-
-# Add the k6 repository to Apt sources
-echo "deb [signed-by=/usr/share/keyrings/k6-archive-keyring.gpg] https://k6.io/repo/deb stable main" | sudo tee /etc/apt/sources.list.d/k6.list
+# sudo gpg -k
+sudo gpg --no-default-keyring --keyring /usr/share/keyrings/k6-archive-keyring.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys C5AD17C747E3415A3642D57D77C6C491D6AC1D69
+echo "deb [signed-by=/usr/share/keyrings/k6-archive-keyring.gpg] https://dl.k6.io/deb stable main" | sudo tee /etc/apt/sources.list.d/k6.list
 sudo apt-get update
-
-# Install k6
 sudo apt-get install -y k6
 echo "✅ k6 installed successfully."
 echo ""
